adea8fb9437fe0c7adb9b5cc47c50e613c5fa3b8
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-09-21 02:44:43 +0530

    created base template

--- invoicer/invoicer/urls.py
+++ invoicer/invoicer/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+	url(r'$', TemplateView.as_view(template_name = "base.html")),
     url(r'^admin/', include(admin.site.urls)),
 ]
