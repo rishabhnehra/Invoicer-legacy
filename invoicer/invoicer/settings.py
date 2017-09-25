aff8bf591741a183622df51bc3c07f6c447e4b93
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-09-26 01:39:39 +0530

    Fixed the product form's GST fields

--- invoicer/invoicer/settings.py
+++ invoicer/invoicer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.4', 'localhost']
 
 
 # Application definition
@@ -113,7 +113,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_roots')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_roots')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
